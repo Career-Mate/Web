ea95cb71f549c77094b839aae9d71267f0c20e35
KOOMINSEOK <rnalstjr1213@naver.com>
2025-01-02 21:54:17 +0900

    Revert commits

--- src/components/buttons/ovalButton/OvalButton.jsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styled/styled';
-
-const OvalButton = ({width, height, backgroundColor,children}) =>{
-    return (
-        <S.Default width = {width} height = {height} backgroundColor= {backgroundColor}>{children}</S.Default>
-    )
-}
-
-export default OvalButton;
--- src/components/buttons/squareButton/SquareButton.jsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styled/styled';
-
-const SquareButton = ({width, height, backgroundColor,children}) =>{
-    return (
-        <S.Default width={width} height = {height} backgroundColor= {backgroundColor}>{children}</S.Default>
-    )
-}
-
-export default SquareButton;
