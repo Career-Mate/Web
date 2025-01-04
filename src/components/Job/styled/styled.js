74a0ebc5f80d631435b7a1d911e32f98213a3537
KOOMINSEOK <rnalstjr1213@naver.com>
2025-01-04 13:20:04 +0900

    style: height, font-weight 변경

--- src/components/Job/styled/styled.js
+++ src/components/Job/styled/styled.js
@@ -1,18 +1,19 @@
 import styled from 'styled-components'
 
-export const Container = styled.span`
+export const Container = styled.div`
     width: fit-content;
-    height: fit-content;
+    height: 20px;
     border-radius: 30px;
     border: 5px solid ${(props)=>props.type === "backend" ? '#FFB861' : '#A8D4BA'};
     background-color: #FFFFFF;
     padding: 8px 8px;  
+`
 
-    //font 설정
+export const Text = styled.span`
     font-family: "Albert Sans";
     text-align: center;
     font-size: 17px;
-    font-weight: 500;
+    font-weight: 600;
     line-height: 20.4px;
     color: ${(props)=>props.type === "backend" ? '#FFB861' : '#A8D4BA'};
 `
