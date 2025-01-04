af7ac2b7fddc1c4715bd0c9061241944dfa5fc6b
KOOMINSEOK <82213236+KOOMINSEOK@users.noreply.github.com>
2025-01-04 14:31:05 +0900

    chore: App.jsx 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import Job from "./components/job/Job";
-import styled from "styled-components";
-
 function App() {
     return (
-        <Box>
-            <Job type="backend"/>
-            <Job type="frontend"/>
-            <Job/>
-        </Box>
-
-    );
+        <>
+        </>
+    )
 }
 export default App;
-
-const Box = styled.div`
-    background-color: #797979;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    width: 100vw;
-    height: 100vh;
-    padding-left: 10px;
-    padding-top: 10px;
-`
