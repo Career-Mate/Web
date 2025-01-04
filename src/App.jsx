29e26dd403d55de8c4f43bcefe875123945667f4
KOOMINSEOK <rnalstjr1213@naver.com>
2025-01-04 14:33:21 +0900

    test: Test 폴더 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import Thumbnail from "./components/Thumbnail/Thumbnail";
-
 function App() {
     return (
         <>
-            <Thumbnail path={'src/assets/thumbnail.png'}/>
         </>
-    );
+    )
 }
 export default App;
