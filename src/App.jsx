460aff56c94cf8e01c35cfa2c07693a169034894
LGY010011 <gylm1111pm@gmail.com>
2025-01-06 23:57:15 +0900

    fix: develop 브랜치와 충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,7 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from "./layout/RootLayout";
-import HomePage from "./pages/HomePage";
-
-const router=createBrowserRouter([
-    {
-        path: '/',
-        element:<RootLayout/>,
-        children: [
-            {
-                index:true,
-                element: <HomePage/>
-            },
-            {
-                path:'career',
-                element: <HomePage/>
-            },
-            {
-                path:'announcement',
-                element: <HomePage/>
-            },
-            {
-                path:'mycareer',
-                element: <HomePage/>
-            },
-        ]
-    }
-])
-
 function App() {
     return (
         <>
-<<<<<<< HEAD
-            <RouterProvider router={router}/>
-        </>
-    );
-=======
         </>
     )
->>>>>>> a81adf9a1ffbcfc152825dc97b9be2580d1db953
 }
 export default App;
