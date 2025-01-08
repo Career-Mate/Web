7b172ed5dfb028d9305fa17da77e9bddec58792e
[SW5] 이아현 <laah0602@gmail.com>
2025-01-08 13:30:43 +0900

    chore: assets png 파일 삭제 및 svg 파일 추가

--- src/components/Menu/Navbar/Navbar.jsx
+++ src/components/Menu/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import * as S from "./styled/styled.js"
 import { useLocation, useNavigate } from "react-router-dom";
-import logo from '../../../assets/career-mate.png'; 
+import logo from '../../../assets/career-mate.svg';
 import LogoutButton from "../../Button/LogoutButton/LogoutButton.jsx";
 import SquareButton from"../../Button/SquareButton/SquareButton.jsx";
 
@@ -20,19 +20,19 @@ const Navbar=()=>{
                 </S.LogoWrapper>
                 <S.Bar>
                     <S.TextWrapper>
-                        <S.Text 
+                        <S.Text
                             $active={isActive('career')}
                             onClick={()=>navigate(`career`)}
                         >
                             커리어 정리하기
                         </S.Text>
-                        <S.Text 
+                        <S.Text
                             $active={isActive('announcement')}
                             onClick={()=>navigate(`announcement`)}
                         >
                             추천 공고
                         </S.Text>
-                        <S.Text 
+                        <S.Text
                             $active={isActive('mycareer')}
                             onClick={()=>navigate(`mycareer`)}
                         >
