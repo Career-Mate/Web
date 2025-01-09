a1b6fdee7c64e146ac0e5232545c2d7835daa07b
LGY010011 <gylm1111pm@gmail.com>
2025-01-10 05:01:41 +0900

    chore: prettier 자동 정리

--- src/components/Menu/SubMenu/SubMenu.jsx
+++ src/components/Menu/SubMenu/SubMenu.jsx
@@ -1,42 +1,29 @@
-import { useState } from "react";
-import * as S from "./styled/styled.js"
+import { useState } from 'react';
+import * as S from './styled/styled.js';
 
-const SubMenu=()=>{
-    const [activeButton, setActiveButton]=useState(0);
+const SubMenu = () => {
+    const [activeButton, setActiveButton] = useState(0);
 
-    const handleButtonClick=(flag)=>{
+    const handleButtonClick = (flag) => {
         setActiveButton(flag);
-    }
+    };
 
-    return(
+    return (
         <S.Conatiner>
-            <S.Button 
-                $isActive={activeButton === 0} 
-                onClick={() => handleButtonClick(0)}
-            >
+            <S.Button $isActive={activeButton === 0} onClick={() => handleButtonClick(0)}>
                 프로필 수정
             </S.Button>
-            <S.Button 
-                $isActive={activeButton === 1} 
-                onClick={() => handleButtonClick(1)}
-            >
+            <S.Button $isActive={activeButton === 1} onClick={() => handleButtonClick(1)}>
                 커리어 정리 폼 확인 및 수정하기
             </S.Button>
-            <S.Button 
-                $isActive={activeButton === 2} 
-                onClick={() => handleButtonClick(2)}
-            >
+            <S.Button $isActive={activeButton === 2} onClick={() => handleButtonClick(2)}>
                 스크랩한 콘텐츠 및 공고 확인하기
             </S.Button>
-            <S.Button 
-                $isActive={activeButton === 3} 
-                onClick={() => handleButtonClick(3)}
-            >
+            <S.Button $isActive={activeButton === 3} onClick={() => handleButtonClick(3)}>
                 SMART 커리어 플래너
             </S.Button>
         </S.Conatiner>
-    )
-}
+    );
+};
 
 export default SubMenu;
-
