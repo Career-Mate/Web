6b003b651005dd277392fb5bae5c0b1036ff6e63
[SW5] 이아현 <laah0602@gmail.com>
2025-01-10 15:46:34 +0900

    feat: 입력 안할 경우만 오류 문구 출력

--- src/components/Input/ProfileInput.jsx
+++ src/components/Input/ProfileInput.jsx
@@ -1,21 +1,28 @@
+import { useState } from 'react';
 import * as S from './styled/styled';
 
 const ProfileInput = ({
     label,
     placeholder,
-    value,
-    onChange,
+    defaultValue = '',
     errorMessage = `${label}을 입력해주세요!`,
     type = 'text',
     size = 'large',
 }) => {
+    const [value, setValue] = useState(defaultValue);
+    const showError = value === '';
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    };
+
     return (
         <S.InputContainer size={size}>
             <S.Label size={size}>{label}</S.Label>
             <S.StyledInputWrapper size={size}>
-                <S.StyledInput type={type} placeholder={placeholder} value={value} onChange={onChange} />
+                <S.StyledInput type={type} placeholder={placeholder} value={value} onChange={handleChange} />
             </S.StyledInputWrapper>
-            <S.ErrorMessage>* {errorMessage}</S.ErrorMessage>
+            {showError && <S.ErrorMessage>* {errorMessage}</S.ErrorMessage>}
         </S.InputContainer>
     );
 };
