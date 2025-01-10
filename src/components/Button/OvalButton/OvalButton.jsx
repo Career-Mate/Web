eb63b8ea4e7e94f68d5176caf4316b1abac24320
Ahyeon Lee <90846224+ahyns62@users.noreply.github.com>
2025-01-10 19:55:27 +0900

    chore: components 폴더 정리

--- src/components/Button/OvalButton/OvalButton.jsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styled/styled';
-
-const OvalButton = ({width, height, padding, backgroundColor, onClick,children}) =>{
-    return (
-        <S.StyledButton width = {width} height = {height} padding ={padding} backgroundColor= {backgroundColor} onClick={onClick}>{children}</S.StyledButton>
-    )
-}
-
-export default OvalButton;
