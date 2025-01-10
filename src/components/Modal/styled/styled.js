b9d0c7a6b4fee77bb837c6b103a74e3347b794e1
rnk00 <ryunamkyong@inha.edu>
2025-01-10 12:56:38 +0900

    chore: 자잘한 수정

--- src/components/Modal/styled/styled.js
+++ src/components/Modal/styled/styled.js
@@ -11,12 +11,10 @@ export const ModalContainer = styled.div`
     box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.25);
     border-radius: 20px;
     padding: 20px;
-
-    /* 내부 콘텐츠 크기에 맞게 조절 */
     display: inline-block;
-    max-width: 90vw; /* 화면 너비의 90%까지만 확장 가능 */
-    max-height: 90vh; /* 화면 높이의 90%까지만 확장 가능 */
-    overflow: auto; /* 내용이 넘칠 경우 스크롤 */
+    max-width: 60vw;
+    max-height: 60vh;
+    overflow: auto;
 `;
 
 export const ModalContent = styled.div`
