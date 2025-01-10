ed2989e88fd5ffd8f8b654d4e96367b775ad9293
[SW5] 이아현 <laah0602@gmail.com>
2025-01-10 15:49:49 +0900

    chore: useInput hooks 폴더 분리

--- src/components/Input/ProfileInput.jsx
+++ src/components/Input/ProfileInput.jsx
@@ -1,26 +1,22 @@
-import { useState } from 'react';
+import useInput from '../../hooks/useInput';
 import * as S from './styled/styled';
 
 const ProfileInput = ({
     label,
     placeholder,
-    defaultValue = '',
     errorMessage = `${label}을 입력해주세요!`,
     type = 'text',
     size = 'large',
+    defaultValue = '',
 }) => {
-    const [value, setValue] = useState(defaultValue);
+    const { value, onChange } = useInput(defaultValue);
     const showError = value === '';
-
-    const handleChange = (e) => {
-        setValue(e.target.value);
-    };
 
     return (
         <S.InputContainer size={size}>
             <S.Label size={size}>{label}</S.Label>
             <S.StyledInputWrapper size={size}>
-                <S.StyledInput type={type} placeholder={placeholder} value={value} onChange={handleChange} />
+                <S.StyledInput type={type} placeholder={placeholder} value={value} onChange={onChange} />
             </S.StyledInputWrapper>
             {showError && <S.ErrorMessage>* {errorMessage}</S.ErrorMessage>}
         </S.InputContainer>
