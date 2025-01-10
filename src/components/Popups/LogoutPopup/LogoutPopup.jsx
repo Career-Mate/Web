74cd865e57b2bb30673e4c7a594dabc5a0ca5fe0
rnk00 <ryunamkyong@inha.edu>
2025-01-10 18:26:48 +0900

    style: popup css 수정

--- src/components/Popups/LogoutPopup/LogoutPopup.jsx
+++ src/components/Popups/LogoutPopup/LogoutPopup.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import * as S from './styled/styled';
 
-const LogoutModal = ({ onCancel, onLogout }) => {
+const LogoutPopup = ({ onCancel, onLogout }) => {
     return (
-        <S.ModalOverlay>
-            <S.ModalContainer>
-                <S.ModalWrapper>
+        <S.PopupOverlay>
+            <S.PopupContainer>
+                <S.PopupWrapper>
                     <S.TextWrapper>
                         <S.StyledText>로그아웃</S.StyledText>
-                        <S.Logout>
-                            정말 로그아웃 하시겠어요? 
-                        </S.Logout>
+                        <S.Logout>정말 로그아웃 하시겠어요?</S.Logout>
                     </S.TextWrapper>
                     <S.ButtonWrapper>
                         <S.CancelButton onClick={onCancel}>취소</S.CancelButton>
                         <S.LogoutButton onClick={onLogout}>로그아웃</S.LogoutButton>
                     </S.ButtonWrapper>
-                </S.ModalWrapper>
-            </S.ModalContainer>
-        </S.ModalOverlay>
+                </S.PopupWrapper>
+            </S.PopupContainer>
+        </S.PopupOverlay>
     );
 };
 
-export default LogoutModal;
+export default LogoutPopup;
