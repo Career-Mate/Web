cbc3f026167919466f01d8a6d06f8931df4c9a2e
rnk00 <ryunamkyong@inha.edu>
2025-01-10 10:30:15 +0900

    feat: modal 생성

--- src/components/Menu/Navbar/styled/styled.js
+++ src/components/Menu/Navbar/styled/styled.js
@@ -1,25 +1,25 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const NavbarContainer = styled.div`
     display: flex;
-    flex-direction: column; 
+    flex-direction: column;
     justify-self: center;
     width: 98%;
     height: 111px;
     position: relative;
 `;
 
-export const Container=styled.div`
+export const Container = styled.div`
     display: flex;
     justify-content: space-between;
-    height:100%;
-    position: relative; 
+    height: 100%;
+    position: relative;
 `;
 
 export const Bar = styled.div`
     display: flex;
-    justify-content:space-between;
-    align-self:flex-end;
+    justify-content: space-between;
+    align-self: flex-end;
     width: 70%;
 `;
 
@@ -27,44 +27,43 @@ export const LogoWrapper = styled.div`
     display: flex;
     justify-content: left;
     align-items: flex-end;
-    width:20%;
+    width: 20%;
 `;
 
-export const Logo=styled.img`
-    width: 100%;  
+export const Logo = styled.img`
+    width: 100%;
     cursor: pointer;
     object-fit: cover;
     clip-path: inset(20% 0 25% 0);
     position: relative;
-    transform: translateY(25%); 
+    transform: translateY(25%);
 `;
 
-
-export const TextWrapper=styled.div`
+export const TextWrapper = styled.div`
     display: flex;
     justify-content: space-between;
 `;
 
 export const Text = styled.div`
-    font-size:16px;
-    cursor: pointer; 
+    font-size: 16px;
+    cursor: pointer;
     display: flex;
-    align-items: center; 
+    align-items: center;
     justify-content: center;
-    height:58px;
-    padding:10px 20px;
-    border-radius:10px 10px 0 0;
+    height: 58px;
+    padding: 10px 20px;
+    border-radius: 10px 10px 0 0;
     box-sizing: border-box;
-    font-weight:${({ $active }) => ($active ? " 700" : "normal")};
-    color:${({ $active }) => ($active ? " #66CCAA" : "black")};
-    border:${({ $active }) => ($active ? "3px solid #80CFB0" : "3px solid transparent")};
+    font-weight: ${({ $active }) => ($active ? ' 700' : 'normal')};
+    color: ${({ $active }) => ($active ? ' #66CCAA' : 'black')};
+    border: ${({ $active }) => ($active ? '3px solid #80CFB0' : '3px solid transparent')};
 `;
 
-export const ButtonWrapper=styled.div`
+export const ButtonWrapper = styled.div`
     display: flex;
     align-items: center;
     box-sizing: border-box;
-    padding:10px 0;
+    padding: 10px 0;
 `;
 
 export const GradientBorder = styled.div`
@@ -72,7 +71,7 @@ export const GradientBorder = styled.div`
     bottom: 0;
     left: 0;
     width: 100%;
-    height: 4px; 
-    border-radius:4px;
-    background: linear-gradient(to right, #66CCAA, #A8D5BA);
+    height: 4px;
+    border-radius: 4px;
+    background: linear-gradient(to right, #66ccaa, #a8d5ba);
 `;
---
+++ src/components/Modal/styled/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+    box-sizing: border-box;
+    position: absolute;
+    width: 768px; //
+    height: 597.32px; //
+    left: 336px; //
+    top: 290px; //
+    background: #ffffff;
+    border: 1px solid #c4c4c4;
+    box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.25);
+    border-radius: 20px;
+`;
