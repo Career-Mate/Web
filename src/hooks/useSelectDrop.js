fe442e4de591b426f586ac3d59be9ac9e5898943
LGY010011 <gylm1111pm@gmail.com>
2025-01-11 12:35:16 +0900

    chore: 학적 상태 데이터 수정

--- src/hooks/useSelectDrop.js
+++ src/hooks/useSelectDrop.js
@@ -4,7 +4,7 @@ export const jobData = ['프론트엔드 개발자', '백엔드 개발자', 'PM(
 
 export const schoolData = ['한양대 에리카', '인하대', '가톨릭대', '한국공대'];
 
-export const academicStatus = ['재학', '휴학', '졸업예정', '졸업', '졸업유예'];
+export const academicStatus = ['재학', '휴학', '졸업예정', '졸업'];
 
 export const useSelectDrop = () => {
     const [isOpen, setIsOpen] = useState(false);
