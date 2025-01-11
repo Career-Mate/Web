01cb4babc43ac43f7d85075b5c1da376352fc216
KOOMINSEOK <rnalstjr1213@naver.com>
2025-01-11 14:30:15 +0900

    chore: 중간 저장

---
+++ src/pages/RecommendContentPage/RecommendContentPage.jsx
@@ -0,0 +1,17 @@
+import * as S from './styled/styeld';
+
+const RecommendContentPage = ({ user = '' }) => {
+    return (
+        <S.Container>
+            <S.TopContainer>
+                    <S.Title>추천 콘텐츠</S.Title>
+                    <S.TextWrapper>
+                        <S.Text><S.Highlight>{user} 메이트님</S.Highlight> 의 커리어와 관련한 콘텐츠에요!</S.Text>
+                        <S.Text> 오른쪽 하단 스크랩 버튼을 클릭하면 나의 커리어에 저장돼요. </S.Text>
+                    </S.TextWrapper>
+            </S.TopContainer>
+        </S.Container>
+    );
+};
+
+export default RecommendContentPage;
