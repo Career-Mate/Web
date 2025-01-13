85196828b10d546304bfb56404ec236957da952b
rnk00 <ryunamkyong@inha.edu>
2025-01-14 04:17:52 +0900

    fix: infoContainer 수정

--- src/components/common/InfoContainer/styled/styled.js
+++ src/components/common/InfoContainer/styled/styled.js
@@ -1,23 +1,92 @@
 import styled from 'styled-components';
 
 export const InfoContainer = styled.div`
-    box-sizing: border-box;
     position: absolute;
-    top: 50%;
-    left: 50%;
+    width: ${(props) => props.$width};
+    height: ${(props) => props.$height};
+    top: ${(props) => props.$top};
+    left: ${(props) => props.$left};
     transform: translate(-50%, -50%);
     background: #ffffff;
     border: 1px solid #c4c4c4;
-    box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.25);
+    box-shadow: 0px 0px 10px 2px rgba(0, 0, 0, 0.23);
     border-radius: 20px;
-    padding: 20px;
-    display: inline-block;
-    max-width: 60vw;
-    max-height: 60vh;
-    overflow: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
 `;
 
-export const ContainerContent = styled.div`
-    padding: 20px;
-    position: relative;
+export const Logo = styled.img`
+    position: absolute;
+    top: calc(-1 * 170px / 2);
+    left: 50%;
+    transform: translateX(-50%);
+    width: 170px;
+    height: 170px;
+    z-index: 10;
+`;
+
+export const TitleText = styled.div`
+    width: 240px;
+    height: 104px;
+    position: absolute;
+    top: calc(-1 * 52px / 2);
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 32px;
+    font-weight: 700;
+    color: #000000;
+    background-color: white;
+    text-align: center;
+    z-index: 10;
+`;
+
+export const ContentWrapper = styled.div`
+    width: 'auto';
+    height: 'auto';
+    top: '50%';
+    left: '50%';
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const TextWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px;
+    gap: 20px;
+    width: auto;
+    height: auto;
+    flex: none;
+    order: 0;
+    align-self: stretch;
+    flex-grow: 0;
+`;
+export const MainText = styled.h2`
+    font-size: 36px;
+    font-weight: 600;
+    color: #000;
+    text-align: center;
+    margin-bottom: 0px;
+`;
+
+export const DetailText = styled.p`
+    font-size: 20px;
+    font-weight: 400;
+    color: #000;
+    text-align: center;
+    margin-bottom: 20px;
+`;
+
+export const ButtonWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+    width: 100%;
+    margin-top: 30px;
 `;
