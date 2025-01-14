66b06db7582d4ac1b2caf8f102f8f1cb0ab73090
[SW5] 이아현 <laah0602@gmail.com>
2025-01-14 13:55:26 +0900

    chore: 사용자 이름과 관심 직무 변수로 분리

--- src/pages/Career/CareerMainPage/CareerMainPage.jsx
+++ src/pages/Career/CareerMainPage/CareerMainPage.jsx
@@ -5,6 +5,8 @@ import LoadingPopup from '../../../components/Popups/LoadingPopup/LoadingPopup';
 import * as S from './styled/styled';
 
 const CareerMainPage = () => {
+    const userName = '김단아';
+    const job = '프론트엔드';
     const [isPopUpVisible, setIsPopUpVisible] = useState(false);
 
     const handleButtonClick = () => {
@@ -21,14 +23,19 @@ const CareerMainPage = () => {
                 <img src={searchIcon} alt="돋보기 아이콘" />
             </S.SearchIcon>
             <InfoContainer>
-                <h2>김단아 메이트에게</h2>
-                <p>직무에 맞는 템플릿을 제공하기 위해 프로필 분석이 필요해요!</p>
+                <h2>{userName} 메이트에게</h2>
+                <p>관심 직무에 맞는 템플릿을 제공하기 위해 프로필 분석이 필요해요!</p>
                 <p>아래 '내 프로필 분석하기'를 클릭해주세요.</p>
                 <button onClick={handleButtonClick}>내 프로필 분석하기</button>
             </InfoContainer>
             {isPopUpVisible && (
                 <LoadingPopup
-                    text={`김단아 메이트님\n 프론트엔드 개발 직무에 맞는 템플릿을 제공해드릴게요!`}
+                    text={
+                        <>
+                            {userName} 메이트님 <br />
+                            {job} 직무에 맞는 템플릿을 제공해드릴게요!
+                        </>
+                    }
                     onCancel={handlePopUpCancel}
                 />
             )}
