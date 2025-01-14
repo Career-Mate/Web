d529e0a67e8ad8504e1a6364551e9c80418909d4
[SW5] 이아현 <laah0602@gmail.com>
2025-01-14 12:19:49 +0900

    feat: 버튼별 페이지 라우팅 설정

--- src/pages/Career/CareerSavePage/CareerSavePage.jsx
+++ src/pages/Career/CareerSavePage/CareerSavePage.jsx
@@ -1,8 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import logo from '../../../assets/common/cm-logo.svg';
 // import InfoContainer from '../../../components/common/InfoContainer/InfoContainer';
 import * as S from './styled/styled';
 
 const CareerSavePage = () => {
+    const navigate = useNavigate();
+
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
+
     return (
         <S.CareerSavePageWrapper>
             <S.LogoWrapper>
@@ -12,9 +19,9 @@ const CareerSavePage = () => {
                 <h2>김단아 메이트님의 커리어가 저장되었습니다.</h2>
                 <p>저장된 템플릿은 '나의 커리어'에서 확인과 수정이 가능해요!</p>
                 <div style={{ display: 'flex', gap: '12px', justifyContent: 'center', marginTop: '24px' }}>
-                    <button>나의 커리어로 이동하기</button>
-                    <button>메인화면으로 돌아가기</button>
-                    <button>채용공고 추천받기</button>
+                    <button onClick={() => handleNavigation('/mycareer')}>나의 커리어로 이동하기</button>
+                    <button onClick={() => handleNavigation('/')}>메인화면으로 돌아가기</button>
+                    <button onClick={() => handleNavigation('/announcement')}>채용공고 추천받기</button>
                 </div>
             </S.InfoContainerWrapper>
         </S.CareerSavePageWrapper>
