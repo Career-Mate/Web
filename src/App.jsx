bd9d76c53ace0b6059d16b8c9078b9c33984aa49
LGY010011 <gylm1111pm@gmail.com>
2025-01-15 19:06:51 +0900

    chore: progress, success  페이지 라우팅 연결

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
 import HomePage from './pages/HomePage';
 import Test from './test/Test';
+import LoginProgressPage from './pages/Main/LoginProgressPage/LoginProgressPage';
+import LoginSuccessPage from './pages/Main/LoginSuccessPage/LoginSuccessPage';
 
 const router = createBrowserRouter([
+    {
+        path: import.meta.env.VITE_OAUTH_REDIRECT_ENDPOINT,
+        element: <LoginProgressPage />,
+    },
     {
         path: '/',
         element: <RootLayout />,
@@ -27,6 +33,10 @@ const router = createBrowserRouter([
             {
                 path: 'test',
                 element: <Test />,
+            },
+            {
+                path: 'login-success',
+                element: <LoginSuccessPage />,
             },
         ],
     },
