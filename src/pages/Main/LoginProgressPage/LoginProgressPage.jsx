a7a4377d2ea89309711d8acfd083c8a9f67ed9f5
LGY010011 <gylm1111pm@gmail.com>
2025-01-16 16:22:15 +0900

    style: 로그인 로딩 시 로고 보이도록 수정

--- src/pages/Main/LoginProgressPage/LoginProgressPage.jsx
+++ src/pages/Main/LoginProgressPage/LoginProgressPage.jsx
@@ -1,13 +1,14 @@
 import * as S from './styled/styled';
 import useOAuthPopUp from '../../../hooks/useOAuthPopUp';
+import Logo from '../../../assets/common/cm.svg';
 
 const LoginProgressPage = () => {
     const { code } = useOAuthPopUp();
 
     return (
         <S.ProgressContainer>
-            <h1>로그인 중...</h1>
-            {code && <p>인증 코드 처리 중입니다...</p>}
+            <object width="300px" type="image/svg+xml" data={Logo}></object>
+            {code && <p>잠시만 기다려주세요...</p>}
         </S.ProgressContainer>
     );
 };
--- src/pages/Main/LoginProgressPage/styled/styled.js
+++ src/pages/Main/LoginProgressPage/styled/styled.js
@@ -4,4 +4,5 @@ export const ProgressContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
 `;
