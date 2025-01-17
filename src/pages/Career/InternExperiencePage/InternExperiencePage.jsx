7379e5d898fa89e8587c882152467660e91fa1ba
[SW5] 이아현 <laah0602@gmail.com>
2025-01-17 14:37:53 +0900

    feat: 컴포넌트 배치

--- src/pages/Career/InternExperiencePage/InternExperiencePage.jsx
+++ src/pages/Career/InternExperiencePage/InternExperiencePage.jsx
@@ -1,10 +1,26 @@
 import Template from '../../../components/common/UserTemplate/Template';
+import ProgressBar from '../../../components/common/ProgressBar/ProgressBar';
+import SquareButton from '../../../components/common/Button/SquareButton/SquareButton';
+import * as S from './styled/styled';
 
 const InternExperiencePage = () => {
     return (
-        <div>
-            <Template jobType="frontend" pageType="internExperience" />
-        </div>
+        <S.PageWrapper>
+            <ProgressBar progression={1} />
+
+            <S.TemplateWrapper>
+                <Template jobType="frontend" pageType="internExperience" />
+            </S.TemplateWrapper>
+
+            <S.ButtonWrapper>
+                <SquareButton width="131px" backgroundColor={'deepgreen'}>
+                    저장
+                </SquareButton>
+                <SquareButton width="131px" backgroundColor={'lightgreen'}>
+                    다음
+                </SquareButton>
+            </S.ButtonWrapper>
+        </S.PageWrapper>
     );
 };
 
