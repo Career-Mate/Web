aa5db3f2d57367e729e4625ed3a17a37574c2814
[SW5] 이아현 <laah0602@gmail.com>
2025-01-18 16:52:58 +0900

    chore: useProgressBar 훅을 이용한 다음 버튼 처리

--- src/pages/Career/InternExperiencePage/InternExperiencePage.jsx
+++ src/pages/Career/InternExperiencePage/InternExperiencePage.jsx
@@ -3,15 +3,17 @@ import ProgressBar from '../../../components/common/ProgressBar/ProgressBar';
 import SquareButton from '../../../components/common/Button/SquareButton/SquareButton';
 import * as S from './styled/styled';
 import { useInternExperience } from './useInternExperience';
+import useProgressBar from '../../../hooks/useProgressBar';
 
 const InternExperiencePage = () => {
     const { data, setData, canSave, handleSave, handleNext } = useInternExperience();
+    const { progression, nextSummaryProgress } = useProgressBar(1);
 
     return (
         <S.PageWrapper>
             <S.HeaderWrapper>
                 <S.Title>1. 인턴 경험</S.Title>
-                <ProgressBar progression={1} />
+                <ProgressBar progression={progression} />
             </S.HeaderWrapper>
 
             <S.TemplateWrapper>
@@ -26,7 +28,7 @@ const InternExperiencePage = () => {
                 <SquareButton width="131px" backgroundColor={'deepgreen'} onClick={handleSave} disabled={!canSave}>
                     저장
                 </SquareButton>
-                <SquareButton width="131px" backgroundColor={'lightgreen'} onClick={handleNext}>
+                <SquareButton width="131px" backgroundColor={'lightgreen'} onClick={nextSummaryProgress}>
                     다음
                 </SquareButton>
             </S.ButtonWrapper>
