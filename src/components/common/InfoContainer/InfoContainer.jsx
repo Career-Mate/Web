e9a0b7b4d57ede70ba16309b76309cf8a3ca201b
rnk00 <139599074+rnk00@users.noreply.github.com>
2025-01-18 15:57:13 +0900

    Update InfoContainer.jsx

--- src/components/common/InfoContainer/InfoContainer.jsx
+++ src/components/common/InfoContainer/InfoContainer.jsx
@@ -1,48 +1,10 @@
 import React from 'react';
-import * as S from './styled/styled';
-import SquareButton from '../Button/SquareButton/SquareButton';
-import Logo from '../../../assets/common/logo.svg';
+import * as S from './styled/styled'; 
 
-const InfoContainer = ({
-    type,
-    width,
-    height,
-    top,
-    left,
-    showLogo = false,
-    showTitleText = false,
-    mainText,
-    detailText,
-    buttons = [],
-}) => {
+const InfoContainer = ({ children }) => {
     return (
-        <S.InfoContainer $width={width} $height={height} $top={top} $left={left}>
-            {showLogo && <S.Logo src={Logo} alt="Logo" $top={top} />}
-            {showTitleText && <S.TitleText $top={top}>프로필 불러오기</S.TitleText>}
-
-            <S.ContentWrapper>
-                {mainText && <S.MainText>{mainText}</S.MainText>}
-                {detailText && <S.DetailText>{detailText}</S.DetailText>}
-
-                {buttons.length > 0 && (
-                    <S.ButtonWrapper>
-                        {buttons.map(({ text, width, height, backgroundColor, padding, onClick }, index) => (
-                            <SquareButton
-                                key={index}
-                                width={width}
-                                height={height}
-                                backgroundColor={backgroundColor}
-                                padding={padding}
-                                onClick={onClick}
-                            >
-                                {text}
-                            </SquareButton>
-                        ))}
-                    </S.ButtonWrapper>
-                )}
-            </S.ContentWrapper>
+        <S.InfoContainer> 
+            <S.ContainerContent>{children}</S.ContainerContent>
         </S.InfoContainer>
     );
 };
-
-export default InfoContainer;
