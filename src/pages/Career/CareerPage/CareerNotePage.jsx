6a76b55f37f94e6124ba1810515314326e38e8c2
[SW5] 이아현 <laah0602@gmail.com>
2025-01-18 15:44:43 +0900

    chore: 템플릿 작성 CareerNotePage 페이지로 관리

---
+++ src/pages/Career/CareerPage/CareerNotePage.jsx
@@ -0,0 +1,32 @@
+import * as S from './styled/styled';
+import HomePage from '../../HomePage';
+import InternExperiencePage from '../InternExperiencePage/InternExperiencePage';
+import { useState } from 'react';
+
+const CareerNotePage = () => {
+    const [activeScreen, setActiveScreen] = useState(0);
+
+    const renderScreen = () => {
+        switch (activeScreen) {
+            case 0:
+                return <InternExperiencePage />;
+            case 1:
+                return <HomePage />;
+            case 2:
+                return <HomePage />;
+            case 3:
+                return <HomePage />;
+            case 4:
+                return <HomePage />;
+            default:
+                return <HomePage />;
+        }
+    };
+
+    return (
+        <S.PageContainer>
+            <S.MainContainer>{renderScreen()}</S.MainContainer>
+        </S.PageContainer>
+    );
+};
+export default CareerNotePage;
---
+++ src/pages/Career/CareerPage/styled/styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    width: 100%;
+    min-height: 100vh;
+    padding: 20px 0;
+`;
+
+export const MainContainer = styled.div`
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    gap: 60px;
+`;
