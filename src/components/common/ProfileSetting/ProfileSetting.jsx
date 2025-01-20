e53c8f46fb9022335a1c05945d54ca30c8874781
LGY010011 <gylm1111pm@gmail.com>
2025-01-20 22:45:56 +0900

    feat: 프로필 설정 컴포넌트 구현

---
+++ src/components/common/ProfileSetting/ProfileSetting.jsx
@@ -0,0 +1,46 @@
+import ProfileInput from '../Input/ProfileInput';
+import SelectDropInput from '../Input/SelectDropInput/SelectDropInput';
+import * as S from './styled/styled';
+import { academicStatus, educationalStatus, jobData } from '../../../hooks/useSelectDrop';
+import SquareButton from '../Button/SquareButton/SquareButton';
+
+const ProfileSetting = () => {
+    return (
+        <S.SettingContainer>
+            <S.InputWrapper>
+                <ProfileInput label={'이름'} placeholder={'이름을 입력하세요'} />
+                <SelectDropInput
+                    label="학력"
+                    width={'633px'}
+                    errorMessage={'학력을'}
+                    optionData={educationalStatus}
+                    boxwidth={'121px'}
+                    boxheight={'196px'}
+                />
+                <ProfileInput label={'학과'} placeholder={'학과'} errorMessage={'학과를 입력해주세요!'} />
+                <SelectDropInput
+                    label="수료 상태"
+                    width={'633px'}
+                    errorMessage={'수료 상태를'}
+                    optionData={academicStatus}
+                    boxwidth={'89px'}
+                    boxheight={'146px'}
+                />
+                <SelectDropInput
+                    label="관심 직무"
+                    width={'633px'}
+                    errorMessage={'관심 직무를'}
+                    optionData={jobData}
+                    boxwidth={'220px'}
+                    boxheight={'146px'}
+                />
+                <ProfileInput label={'이메일'} type="email" placeholder={'이메일을 입력하세요'} />
+            </S.InputWrapper>
+            <SquareButton width={'375px'} height={'60px'}>
+                프로필 설정하기
+            </SquareButton>
+        </S.SettingContainer>
+    );
+};
+
+export default ProfileSetting;
---
+++ src/components/common/ProfileSetting/styled/styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const SettingContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 633px;
+    gap: 65px;
+`;
+
+export const InputWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
