55e90f0e8d40c78fae25d87154ab2ecb7e7bfbc0
[SW5] 이아현 <laah0602@gmail.com>
2025-01-20 20:49:28 +0900

    chore: CareerNotePage 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import RecommendMainPage from './pages/Recommend/RecommnedMainPage/RecommendMainPage';
 import Test from './test/Test';
 import MainPage from './pages/Main/MainPage/MainPage';
+import CareerNotePage from './pages/Career/CareerPage/CareerNotePage';
 
 const router = createBrowserRouter([
     {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
             {
                 path: 'career',
                 element: <HomePage />,
+            },
+            {
+                path: 'career/note',
+                element: <CareerNotePage />,
             },
             {
                 path: 'announcement',
