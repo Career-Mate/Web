89edacc0fa7d902c477017eee73fd510d5679ada
rnk00 <ryunamkyong@inha.edu>
2025-01-21 20:07:33 +0900

    chore: 버튼 onClick 설정

--- src/pages/Recommend/RecommnedMainPage/RecommendMainPage.jsx
+++ src/pages/Recommend/RecommnedMainPage/RecommendMainPage.jsx
@@ -1,6 +1,10 @@
 import InfoContainer from '../../../components/common/InfoContainer/InfoContainer';
+import { useNavigate } from 'react-router-dom';
 
-const RecommendMainPage = ({ userName }) => {
+const RecommendMainPage = () => {
+    const userName = '김단아';
+    const navigate = useNavigate();
+
     return (
         <InfoContainer
             type="contentOnly"
@@ -9,27 +13,31 @@ const RecommendMainPage = ({ userName }) => {
             top="271px"
             showLogo={false}
             showTitleText={false}
-            mainText="김단아 메이트님에게"
+            mainText={`${userName} 메이트님에게`}
             detailText={`적합한 공고 추천을 위해
                     커리어 정리 템플릿 분석이 필요해요!
-                    아래 '템플릿 불러오기'를 클릭해주세요.
+                    아래 '추천 공고 불러오기'를 클릭해주세요.
                     
                     지원하기 전 직무 관련 콘텐츠를 보고 싶다면
                     아래 '콘텐츠 보러가기'를 클릭해주세요.`}
             buttons={[
                 {
-                    text: '템플릿 불러오기',
+                    text: '추천 공고 불러오기',
                     width: '327px',
                     height: '60px',
                     backgroundColor: 'deepgreen',
-                    onClick: () => console.log('템플릿 불러오기 클릭'),
+                    onClick: () => {
+                        navigate('/recommend/job');
+                    },
                 },
                 {
                     text: '콘텐츠 보러가기',
                     width: '327px',
                     height: '60px',
                     backgroundColor: 'green',
-                    onClick: () => console.log('콘텐츠 보러가기 클릭'),
+                    onClick: () => {
+                        navigate('/recommend/content');
+                    },
                 },
             ]}
         />
