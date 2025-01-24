2b7f20cd147f6dd8f099f2d73cec2f9734e845fe
LGY010011 <gylm1111pm@gmail.com>
2025-01-24 16:35:43 +0900

    chore: useSelectDrop hooks에서 데이터 제거

--- src/hooks/useSelectDrop.js
+++ src/hooks/useSelectDrop.js
@@ -1,11 +1,5 @@
 import { useState } from 'react';
 
-export const jobData = ['프론트엔드 개발자', '백엔드 개발자', 'PM(Product/Project Manager)', 'Designer'];
-
-export const academicStatus = ['재학', '휴학', '졸업', '수료'];
-
-export const educationalStatus = ['중학교 이하', '고등학교', '전문대학', '대학교', '석사', '박사'];
-
 export const useSelectDrop = (defaultValue) => {
     const [isOpen, setIsOpen] = useState(false);
     const [selectedOption, setSelectedOption] = useState(defaultValue);
