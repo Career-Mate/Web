c00d0e4eccdea1d428222539bfc4e40d20c541e7
[SW5] 이아현 <laah0602@gmail.com>
2025-01-25 15:20:56 +0900

    refactor: TextTemplate 렌더링 최적화

--- src/components/common/TextTemplate/TextTemplate.jsx
+++ src/components/common/TextTemplate/TextTemplate.jsx
@@ -1,11 +1,13 @@
 import * as S from './styled/styled';
+import { useMemo } from 'react';
 import { useTemplateData } from '../../../hooks/useTemplateData';
 import { textTemplateData } from '../../../data/textTemplateData';
 import UnderlineButton from '../Button/UnderlineButton/UnderlineButton';
 
-const TextTemplate = ({ data: externalData, onDataChange }) => {
-    const initialData = textTemplateData;
+const TextTemplate = ({ jobType = 'frontend', pageType = 'skills', data: externalData, onDataChange }) => {
+    const initialData = textTemplateData[pageType]?.[jobType] || [];
     const { handleInputChange, data, clearAll } = useTemplateData(externalData || initialData, onDataChange);
+    const memoizedData = useMemo(() => data, [data]);
 
     const autoResize = (textarea) => {
         if (textarea) {
@@ -16,7 +18,7 @@ const TextTemplate = ({ data: externalData, onDataChange }) => {
 
     return (
         <div>
-            {data.map((section, sectionIndex) => (
+            {memoizedData.map((section, sectionIndex) => (
                 <S.TemplateWrapper key={sectionIndex}>
                     <S.TemplateTitle>{section.title}</S.TemplateTitle>
                     <S.TemplateTable>
