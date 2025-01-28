b4a33992ef280d219e3965b0b3caae2708c45891
LGY010011 <gylm1111pm@gmail.com>
2025-01-28 15:40:17 +0900

    feat: 로그인 url 연결

--- src/pages/Main/LoginPage/LoginPage.jsx
+++ src/pages/Main/LoginPage/LoginPage.jsx
@@ -2,17 +2,9 @@ import * as S from './styled/styled';
 import Naver from '../../../assets/LoginPage/naver.svg';
 import Kakao from '../../../assets/LoginPage/kakao.svg';
 import InfoContainer from '../../../components/common/InfoContainer/InfoContainer';
-import useLoginPopUp from '../../../hooks/useLoginPopUp';
-import { useEffect } from 'react';
 
 const LoginPage = () => {
-    const { open, code } = useLoginPopUp();
-
-    useEffect(() => {
-        if (code) {
-            console.log('받은 인증 코드:', code);
-        }
-    }, [code]);
+    const apiURL = `${import.meta.env.VITE_BACK_URL}/oauth2/authorization`;
 
     return (
         <InfoContainer
@@ -25,13 +17,13 @@ const LoginPage = () => {
                 <S.Container>
                     <S.Text>SNS로 시작하기</S.Text>
                     <S.SocialButtonWrapper>
-                        <a href="/oauth2/authorization/naver">
+                        <a href={`${apiURL}/naver`}>
                             <S.SocialButton $type="naver">
                                 <img src={Naver} alt="naver"></img>
                                 네이버로 시작하기
                             </S.SocialButton>
                         </a>
-                        <a href="/oauth2/authorization/kakao">
+                        <a href={`${apiURL}/kakao`}>
                             <S.SocialButton $type="kakao">
                                 <img src={Kakao} alt="kakao"></img>
                                 카카오로 시작하기
