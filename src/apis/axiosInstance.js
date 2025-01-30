ecf9b937d1fde2a7ccabafafc4817c42218b00fd
[SW5] 이아현 <laah0602@gmail.com>
2025-01-30 17:09:46 +0900

    setting: axiosInstance 수정

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
@@ -8,5 +8,4 @@ const apiClient = axios.create({
         'Content-Type': 'application/json',
     },
 });
-
 export default apiClient;
