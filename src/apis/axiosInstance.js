3627a9b045d7a518b523a9324356d12549fbf216
KOOMINSEOK <rnalstjr1213@naver.com>
2025-01-31 23:53:25 +0900

    chore: 중간저장

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -4,20 +4,21 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
-const TEMP_TOKEN = import.meta.env.VITE_KOO_TOKEN;
+// const TEMP_TOKEN = import.meta.env.VITE_KOO_TOKEN;
 
-apiClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem("accessToken") || TEMP_TOKEN;
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
-});
+// apiClient.interceptors.request.use((config) => {
+//     const token = localStorage.getItem("accessToken") || TEMP_TOKEN;
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+// }, (error) => {
+//     return Promise.reject(error);
+// });
 export default apiClient;
