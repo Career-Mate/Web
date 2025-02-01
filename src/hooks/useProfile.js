80fb7a44e6378b31a6ffeaa576e41362561b4703
LGY010011 <gylm1111pm@gmail.com>
2025-02-01 13:49:37 +0900

    chore: 사용 안 하는 import 제거

--- src/hooks/useProfile.js
+++ src/hooks/useProfile.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { educationLevel, educationStatus, jobData } from '../data/profileData';
 
 export const useProfile = (initialData) => {
     const [profile, setProfile] = useState(initialData);
