ee28fe5103b58a00af277497f6f695fd57268003
[SW5] 이아현 <laah0602@gmail.com>
2025-02-01 14:53:42 +0900

    chore: axiosInstance에 임시 인증 토큰 추가

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const TEMP_TOKEN = import.meta.env.VITE_TEMP_TOKEN;
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
+        Authorization: TEMP_TOKEN,
     },
 });
 export default apiClient;
