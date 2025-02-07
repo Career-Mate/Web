d3e7e38a54cb01d187f24de604335523d02536f2
LGY010011 <gylm1111pm@gmail.com>
2025-02-07 23:30:34 +0900

    fix: Zustand 내부 hooks 호출 제거

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -2,7 +2,6 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { sanitizeProfile } from '../utils/Profile/ProfileMapper';
 import { profileEmptyData } from '../data/profileData';
-import { useQueryClient } from '@tanstack/react-query';
 
 export const useAuthStore = create(
     persist(
@@ -20,8 +19,6 @@ export const useAuthStore = create(
                     user: profileEmptyData,
                 }),
                     sessionStorage.removeItem('authStorage');
-                const queryClient = useQueryClient();
-                queryClient.removeQueries('profile');
             },
             fetchUser: (userData) =>
                 set({
