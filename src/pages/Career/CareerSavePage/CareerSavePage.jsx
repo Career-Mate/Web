94bba9167bfac4f264ff6527e0345f5568a6f596
[SW5] 이아현 <laah0602@gmail.com>
2025-02-08 02:22:55 +0900

    style: CareerSavePage 문구 및 버튼 변경

--- src/pages/Career/CareerSavePage/CareerSavePage.jsx
+++ src/pages/Career/CareerSavePage/CareerSavePage.jsx
@@ -14,37 +14,29 @@ const CareerSavePage = () => {
         <S.CareerSavePageWrapper>
             <InfoContainer
                 type="logoWithContent"
-                width="981px"
-                height="393.27px"
+                width="815px"
+                height="372px"
                 top="327.73px"
                 showLogo={true}
                 showTitleText={false}
                 mainText={`${userName} 메이트님의 커리어가 저장되었습니다.`}
-                detailText="저장된 템플릿은 ‘나의 커리어'에서 확인과 수정이 가능해요!"
+                detailText="정리한 커리어를 바탕으로 채용 공고 추천도 받을 수 있어요!"
                 buttons={[
                     {
-                        text: '나의 커리어로 이동하기',
-                        width: '280px',
-                        height: '60px',
-                        padding: '0px',
-                        backgroundColor: 'green',
-                        onClick: () => handleNavigation('/mycareer'),
-                    },
-                    {
-                        text: '메인화면으로 돌아가기',
-                        width: '271px',
-                        height: '60px',
-                        padding: '0px',
-                        backgroundColor: 'grey',
-                        onClick: () => handleNavigation('/'),
-                    },
-                    {
                         text: '채용 공고 추천 받기',
-                        width: '271px',
-                        height: '60px',
+                        width: '330px',
+                        height: '57px',
                         padding: '0px',
                         backgroundColor: 'deepgreen',
                         onClick: () => handleNavigation('/recommend'),
+                    },
+                    {
+                        text: '메인화면으로 돌아가기',
+                        width: '330px',
+                        height: '57px',
+                        padding: '0px',
+                        backgroundColor: 'grey',
+                        onClick: () => handleNavigation('/'),
                     },
                 ]}
             />
