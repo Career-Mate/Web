5ef717ec2ab4ae8b08ab0d79463db5080acb036c
LGY010011 <gylm1111pm@gmail.com>
2025-02-07 14:27:30 +0900

    feat: 쿠키 포함 설정

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const TEMP_TOKEN = import.meta.env.VITE_TEMP_TOKEN;
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
-        Authorization: TEMP_TOKEN,
     },
+    withCredentials: true,
 });
 export default apiClient;
