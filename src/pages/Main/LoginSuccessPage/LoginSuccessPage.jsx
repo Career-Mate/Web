432e3631725b6f7fd852b08f52e3ea20659b2982
LGY010011 <gylm1111pm@gmail.com>
2025-02-08 22:15:29 +0900

    fix: LoginSuccessPage 조건 제거

--- src/pages/Main/LoginSuccessPage/LoginSuccessPage.jsx
+++ src/pages/Main/LoginSuccessPage/LoginSuccessPage.jsx
@@ -1,55 +1,31 @@
 import { useNavigate } from 'react-router-dom';
 import InfoContainer from '../../../components/common/InfoContainer/InfoContainer';
-import { useAuthStore } from '../../../store/authStore';
 
 const LoginSuccessPage = () => {
-    const { isLogin, user } = useAuthStore();
     const navigate = useNavigate();
 
     return (
         <>
-            {isLogin === true ? (
-                <InfoContainer
-                    type="logoWithContent"
-                    width="756px"
-                    height="394.13px"
-                    top="286.87px"
-                    showLogo={true}
-                    showTitleText={false}
-                    mainText="로그인 성공"
-                    detailText={`${user.name} 메이트님에게 최적의 서비스를 제공할 수 있도록 프로필을 설정해주세요`}
-                    buttons={[
-                        {
-                            text: '지금 바로 프로필 설정하기',
-                            width: '375px',
-                            height: '60px',
-                            padding: '18px',
-                            backgroundColor: 'rgba(43, 157, 143, 1)',
-                            onClick: () => navigate('/profile'),
-                        },
-                    ]}
-                />
-            ) : (
-                <InfoContainer
-                    type="logoWithContent"
-                    width="756px"
-                    height="394.13px"
-                    top="286.87px"
-                    showLogo={true}
-                    showTitleText={false}
-                    mainText="로그인이 필요합니다"
-                    buttons={[
-                        {
-                            text: '지금 바로 로그인 하기',
-                            width: '375px',
-                            height: '60px',
-                            padding: '18px',
-                            backgroundColor: 'rgba(43, 157, 143, 1)',
-                            onClick: () => navigate('/login'),
-                        },
-                    ]}
-                />
-            )}
+            <InfoContainer
+                type="logoWithContent"
+                width="756px"
+                height="394.13px"
+                top="286.87px"
+                showLogo={true}
+                showTitleText={false}
+                mainText="로그인 성공"
+                detailText={`최적의 서비스를 제공할 수 있도록 프로필을 설정해주세요`}
+                buttons={[
+                    {
+                        text: '지금 바로 프로필 설정하기',
+                        width: '375px',
+                        height: '60px',
+                        padding: '18px',
+                        backgroundColor: 'rgba(43, 157, 143, 1)',
+                        onClick: () => navigate('/profile'),
+                    },
+                ]}
+            />
         </>
     );
 };
