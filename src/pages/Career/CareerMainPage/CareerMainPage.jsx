aa4f6bc1ba616a9d3df6560b3223fca3afc4b333
[SW5] 이아현 <laah0602@gmail.com>
2025-02-09 01:19:39 +0900

    chore: CareerMainPage 팝업 setTimeout 1초로 단축

--- src/pages/Career/CareerMainPage/CareerMainPage.jsx
+++ src/pages/Career/CareerMainPage/CareerMainPage.jsx
@@ -17,7 +17,7 @@ const CareerMainPage = () => {
         timeoutId.current = setTimeout(() => {
             handlePopUpCancel();
             navigate('/career/note');
-        }, 1500);
+        }, 1000);
     };
 
     const handlePopUpCancel = () => {
