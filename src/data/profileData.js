bf9df63063dc2e4673404565d194613bffa61f4d
LGY010011 <gylm1111pm@gmail.com>
2025-02-08 23:56:56 +0900

    chore: profileInitialData 안 쓰는 데이터 제거

--- src/data/profileData.js
+++ src/data/profileData.js
@@ -1,12 +1,3 @@
-export const profileInitialData = {
-    name: '김단아',
-    email: 'test@email.com',
-    educationLevel: '대학교',
-    major: '컴퓨터공학과',
-    educationStatus: '재학',
-    job: '프론트엔드 개발자',
-};
-
 export const profileEmptyData = {
     name: '',
     email: '',
