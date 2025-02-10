a7df680df9434d07d467f4879d4b07a354557254
LGY010011 <gylm1111pm@gmail.com>
2025-02-11 00:02:17 +0900

    feat: axios interceptors로 POST /auth/refresh 구현

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { refreshToken } from './Auth/AuthApi';
+import { useHandleLogout } from './Auth/useHandleLogout';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const TEMP_TOKEN = import.meta.env.VITE_TEMP_TOKEN;
@@ -10,4 +12,28 @@ const apiClient = axios.create({
         Authorization: TEMP_TOKEN,
     },
 });
+
+apiClient.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        const statusCode = error.response?.status;
+        const errorCode = error.response?.data?.code;
+        const handleLogout = useHandleLogout();
+        if (
+            ((statusCode === 4001 && errorCode === 'ETK001') || (statusCode === 4003 && errorCode === 'ETK004')) &&
+            !originalRequest._retry
+        ) {
+            originalRequest._retry = true;
+            const success = await refreshToken();
+            if (success) {
+                return apiClient(originalRequest);
+            } else {
+                handleLogout();
+            }
+        }
+        return Promise.reject(error);
+    },
+);
+
 export default apiClient;
