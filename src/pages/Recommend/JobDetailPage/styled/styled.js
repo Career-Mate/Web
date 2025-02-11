e90a71b13978296042202ca05bbc39d096dc5bbf
KOOMINSEOK <rnalstjr1213@naver.com>
2025-02-12 00:22:16 +0900

    chore: 토끼로고 경로 변경

--- src/pages/Recommend/JobDetailPage/styled/styled.js
+++ src/pages/Recommend/JobDetailPage/styled/styled.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import RabbitLogo from '../../../../assets/JobDetailPage/rabbit-logo.svg';
+import RabbitLogo from '../../../../../public/assets/rabbit-logo.svg';
 export const PageContainer = styled.div`
     width: 100%;
     height: fit-content;
