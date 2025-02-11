aed1a959f4ce8af2b9c75aad839d050428358b4a
KOOMINSEOK <rnalstjr1213@naver.com>
2025-02-12 00:26:43 +0900

    chore: 말풍선 경로 변경

--- src/pages/MyCareer/SmartPlannerPage/styled/styled.js
+++ src/pages/MyCareer/SmartPlannerPage/styled/styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import SpeechBubble from '../../../../assets/common/speech-bubble.svg';
-import MobileSpeechBubble from '../../../../assets/common/mobile-speech-bubble.svg';
+import SpeechBubble from '../../../../../public/assets/speech-bubble.svg';
+import MobileSpeechBubble from '../../../../../public/assets/mobile-speech-bubble.svg';
 
 export const MainContainer = styled.div`
     height: fit-content;
