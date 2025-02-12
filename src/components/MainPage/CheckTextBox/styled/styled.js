7ffc246681732fa2c0217c9e21597ed529324c9d
LGY010011 <gylm1111pm@gmail.com>
2025-02-12 22:49:36 +0900

    style: CheckTextBox 컴포넌트 반응형

--- src/components/MainPage/CheckTextBox/styled/styled.js
+++ src/components/MainPage/CheckTextBox/styled/styled.js
@@ -14,20 +14,61 @@ export const IconWrapper = styled.div`
     background: #ffffff;
     box-shadow: 2px 2px 0px 1px rgba(0, 0, 0, 0.25);
     border-radius: 10px;
+    box-sizing: border-box;
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+
+    @media (max-width: 1024px) {
+        width: 40px;
+        height: 40px;
+        font-size: 14px;
+        border-radius: 10px;
+        padding: 5px 3px;
+    }
 `;
 
 export const Text = styled.div`
     width: 758px;
+    height: 68px;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     font-size: 20px;
     border-radius: 20px;
-    padding: 18px 26px;
     background-color: white;
+    text-align: center;
+    word-break: break-word;
+    white-space: normal;
     box-shadow: inset 0px 0px 8.3px 1px rgba(0, 0, 0, 0.25);
+
+    padding: 0 10px;
+    box-sizing: border-box;
+
+    span {
+        display: inline;
+        word-break: break-word;
+    }
     strong {
         font-weight: 700;
+        display: inline;
+        word-break: break-word;
+    }
+
+    @media (max-width: 1024px) {
+        width: 542px;
+        height: 40px;
+        font-size: 14px;
+        border-radius: 10px;
+    }
+
+    @media (max-width: 430px) {
+        width: 307px;
+        height: 38px;
+        font-size: 10px;
+        border-radius: 10px;
     }
 `;
