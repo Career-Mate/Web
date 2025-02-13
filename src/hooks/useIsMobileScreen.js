cb731fbe436b27a9cc48364b0ed8de65c6fbdb8d
KOOMINSEOK <rnalstjr1213@naver.com>
2025-02-14 03:27:33 +0900

    feat: JobPage와 ContentPage 로딩화면 구현 및 useIsMobileScreen 훅 개선

--- src/hooks/useIsMobileScreen.js
+++ src/hooks/useIsMobileScreen.js
@@ -1,18 +1,20 @@
 import { useState, useEffect } from 'react';
 
 const useIsMobileScreen = (breakpoint = 390) => {
-    const [isLargeScreen, setIsLargeScreen] = useState(window.outerWidth <= breakpoint);
+    const [isMobileScreen, setIsMobileScreen] = useState(() => window.innerWidth <= breakpoint);
 
     useEffect(() => {
         const handleResize = () => {
-            setIsLargeScreen(window.outerWidth <= breakpoint);
+            setIsMobileScreen(window.innerWidth <= breakpoint);
         };
+
+        handleResize();
 
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
     }, [breakpoint]);
 
-    return isLargeScreen;
+    return isMobileScreen;
 };
 
 export default useIsMobileScreen;
--- src/pages/Recommend/RecommendPage/RecommendContentPage.jsx
+++ src/pages/Recommend/RecommendPage/RecommendContentPage.jsx
@@ -2,6 +2,7 @@ import * as S from './styled/styled';
 import ContentCard from '../../../components/common/Card/ContentCard/ContentCard';
 import JobBox from '../../../components/Recommend/JobBox/JobBox';
 import OvalButton from '../../../components/common/Button/OvalButton/OvalButton';
+import ContentCardSkeleton from '../../../components/SkeletonUi/ContentCardSkeleton/ContentCardSkeleton';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useGetRecommendContents } from '../../../apis/Content/ContentApi';
@@ -11,6 +12,8 @@ const RecommendContentPage = ({ user }) => {
     const navigate = useNavigate();
     const queryClient = useQueryClient();
 
+    const numbers = Array.from({ length: 3 }, (_, i) => i + 1);
+
     const { data, isLoading, error } = useGetRecommendContents();
     const contents = data?.contents || [];
 
@@ -18,7 +21,7 @@ const RecommendContentPage = ({ user }) => {
         window.scrollTo(0, 0);
     }, []);
 
-    if (isLoading) return <div>Loading...</div>;
+    // if (isLoading) return <div>Loading...</div>;
     if (error) return <div>error</div>;
 
     return (
@@ -31,16 +34,18 @@ const RecommendContentPage = ({ user }) => {
                 <S.Text> 오른쪽 하단 스크랩 버튼을 클릭하면 나의 커리어에 저장돼요. </S.Text>
             </S.TextWrapper>
             <S.CardWrapper>
-                {contents.map((content) => (
-                    <ContentCard
-                        key={content.id}
-                        id={content.id}
-                        contentName={content.contentName}
-                        thumbnail={content.thumbnail}
-                        url={content.url}
-                        isScrapped={content.isScrapped}
-                    />
-                ))}
+                {isLoading
+                    ? contents.map((content) => (
+                          <ContentCard
+                              key={content.id}
+                              id={content.id}
+                              contentName={content.contentName}
+                              thumbnail={content.thumbnail}
+                              url={content.url}
+                              isScrapped={content.isScrapped}
+                          />
+                      ))
+                    : numbers.map((number) => <ContentCardSkeleton />)}
             </S.CardWrapper>
             <S.ButtonContainer>
                 <OvalButton
