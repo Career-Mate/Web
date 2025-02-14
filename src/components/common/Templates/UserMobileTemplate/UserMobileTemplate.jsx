131f9ed2b03c5c405cb7728d8a205f2b4e9d69a0
[SW5] 이아현 <laah0602@gmail.com>
2025-02-14 19:43:23 +0900

    feat: UserMobileTemplate 컴포넌트서 사용할 컴포넌트들 추가

--- src/components/common/Templates/UserMobileTemplate/UserMobileTemplate.jsx
+++ src/components/common/Templates/UserMobileTemplate/UserMobileTemplate.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
 import TemplateCategory from '../../Templates/TemplateCategory/TemplateCategory.jsx';
 import MobilePagination from '../../Pagination/MobilePagination/MobilePagination.jsx';
+import MobileTextArea from '../../MobileTextarea/MobileTextarea.jsx';
+import UnderlineButton from '../../Button/UnderlineButton/UnderlineButton';
+import SquareButton from '../../Button/SquareButton/SquareButton.jsx';
 
 const UserMobileTemplate = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+
     return (
         <>
             <TemplateCategory />
-            <MobilePagination totalPages={2} currentPage={2} setCurrentPage={2} />
+
+            <h5>※ 최대 2개까지 작성할 수 있어요.</h5>
+
+            <div style={{ padding: '16px' }}>
+                <h2>현재 페이지: {currentPage}</h2>
+                <MobileTextArea label={'1. 직무명'} />
+            </div>
+
+            <UnderlineButton fontSize="12px">전체 내용 삭제하기</UnderlineButton>
+
+            <SquareButton width="131px" backgroundColor={'deepgreen'}>
+                저장
+            </SquareButton>
+
+            <MobilePagination totalPages={2} currentPage={currentPage} setCurrentPage={setCurrentPage} />
         </>
     );
 };
