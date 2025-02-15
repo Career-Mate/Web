22e6c0f1464b2e8bfc54adb6a9f87265bfb834eb
KOOMINSEOK <rnalstjr1213@naver.com>
2025-02-15 14:56:07 +0900

    chore: 주석 삭제

--- src/pages/Recommend/RecommendPage/RecommendContentPage.jsx
+++ src/pages/Recommend/RecommendPage/RecommendContentPage.jsx
@@ -21,7 +21,6 @@ const RecommendContentPage = ({ user }) => {
         window.scrollTo(0, 0);
     }, []);
 
-    // if (isLoading) return <div>Loading...</div>;
     if (error) return <div>error</div>;
 
     return (
