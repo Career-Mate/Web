5bae21159194d25cfc413598f5073b20d99bebed
LGY010011 <gylm1111pm@gmail.com>
2025-02-15 17:39:11 +0900

    feat: 쿼리파라미터로 로그인 체크

--- src/pages/Main/MainPage/MainPage.jsx
+++ src/pages/Main/MainPage/MainPage.jsx
@@ -9,8 +9,7 @@ import CheckTextBox from '../../../components/MainPage/CheckTextBox/CheckTextBox
 import InterviewBox from '../../../components/MainPage/InterviewBox/InterviewBox';
 import Card from '../../../components/MainPage/Card/Card';
 import OvalButton from '../../../components/common/Button/OvalButton/OvalButton';
-import { useNavigate } from 'react-router-dom';
-import { useFetchProfile } from '../../../apis/Profile/useProfileApi';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../../../store/authStore';
 import { useEffect } from 'react';
 import useIsMobileScreen from '../../../hooks/useIsMobileScreen';
@@ -38,14 +37,16 @@ const cards = [
 
 const MainPage = () => {
     const navigate = useNavigate();
-    const { data, error } = useFetchProfile();
     const { isLogin, login } = useAuthStore();
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const userName = queryParams.get('name');
 
     useEffect(() => {
-        if (data) {
-            login(data);
+        if (userName) {
+            login({ name: userName });
         }
-    }, [data]);
+    }, [userName, login]);
 
     const isMobileScreen = useIsMobileScreen(430);
     const buttonWidth = isMobileScreen ? '240px' : '400px';
