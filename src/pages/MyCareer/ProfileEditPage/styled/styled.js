e9b3013792f2021a343bd6280086c1139838ce0e
LGY010011 <gylm1111pm@gmail.com>
2025-02-17 00:33:42 +0900

    style: ProfileEditPage 반응형 UI

--- src/pages/MyCareer/ProfileEditPage/styled/styled.js
+++ src/pages/MyCareer/ProfileEditPage/styled/styled.js
@@ -4,28 +4,47 @@ export const EditContainer = styled.div`
     height: fit-content;
     display: flex;
     flex-direction: column;
+
+    @media (max-width: 431px) {
+        margin-left: 0;
+        align-items: center;
+        margin-bottom: 80px;
+    }
 `;
 
 export const NoticeWrapper = styled.div`
     display: flex;
     flex-direction: column;
     margin-bottom: 30px;
+
+    @media (max-width: 431px) {
+        margin-top: 50px;
+        text-align: center;
+    }
 `;
 
 export const NoticeTitle = styled.h1`
     font-size: 20px;
     font-weight: 700;
+
+    @media (max-width: 431px) {
+        font-size: 16px;
+    }
 `;
 
 export const NoticeDetail = styled.div`
     font-size: 16px;
     font-weight: 400;
+
+    @media (max-width: 431px) {
+        font-size: 14px;
+    }
 `;
 
 export const ContentWrapper = styled.div`
-    width: 633px;
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 20px;
+    width: fit-content;
 `;
