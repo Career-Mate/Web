4d5856818161e7c3487ef1728a2047f1a89cb1c0
[SW5] 이아현 <laah0602@gmail.com>
2025-02-18 22:28:11 +0900

    style: CareerMainPage 및 CareerSavePage min-height 제거

--- src/pages/Career/CareerMainPage/styled/styled.js
+++ src/pages/Career/CareerMainPage/styled/styled.js
@@ -5,7 +5,6 @@ export const CareerMainPageWrapper = styled.div`
     flex-direction: column;
     align-items: flex-start;
     justify-content: flex-start;
-    min-height: calc(100vh - 100px);
     padding: 32px 16px;
     background-color: white;
     position: relative;
